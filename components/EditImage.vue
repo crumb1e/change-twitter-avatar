03ae5a697a48d0ffabaa1f8e2a1882236f3c9e48
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-05-09 15:31:18 +0100

    init

---
+++ components/EditImage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="p-2 text-3xl">
+            Change my pic pls
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EditImage',
+
+    data() {
+        return {
+            test: ''
+        }
+    }
+}
+</script>
